357f3c571f1e86b37d6bc12f95f01015d95f97ee
jjmm0312 <198032@naver.com>
2020-08-05 22:52:49 +0900

    Add DataManager

---
+++ DataManager/src/test/java/vaninside/smartelecsystem/datamanager/DataManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package vaninside.smartelecsystem.datamanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
