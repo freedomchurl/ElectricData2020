09ff4d4dd6413fb65e46092449469527fd1592a7
jjmm0312 <198032@naver.com>
2020-08-11 20:22:28 +0900

    Add Redis Process

--- DataManager/src/main/java/vaninside/smartelecsystem/datamanager/service/DataService.java
+++ DataManager/src/main/java/vaninside/smartelecsystem/datamanager/service/DataService.java
@@ -1,5 +1,8 @@
 package vaninside.smartelecsystem.datamanager.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.eclipse.paho.client.mqttv3.IMqttClient;
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.MqttCallback;
@@ -8,10 +11,25 @@ import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.eclipse.paho.client.mqttv3.MqttPersistenceException;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import vaninside.smartelecsystem.datamanager.controller.DataController;
 import vaninside.smartelecsystem.datamanager.dao.DataDao;
@@ -19,16 +37,11 @@ import vaninside.smartelecsystem.datamanager.dao.DataDao;
 @Service
 public class DataService implements IDataService{
 
-	public static String topic = "topic";
+	public static String topic = "topic2";
 	
 	@Autowired
 	DataDao dao;
-
-	@Override
-	public boolean saveUserData(String msg) {
-		return dao.insertInput(msg);
-	}
-
+	
 	@Override
 	public boolean sendControlData(String msg) {
 		MqttMessage mqttMessage = new MqttMessage();
@@ -44,5 +57,42 @@ public class DataService implements IDataService{
 		}
 		
 		return dao.insertOutput(msg);
+	}
+	
+	public String predict(String msg) throws JsonProcessingException{
+
+		System.out.println(msg);
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		RestTemplate restTemplate = new RestTemplate();
+		
+		String url = "http://localhost:5000/predict";
+		
+		HttpEntity<String> entity = new HttpEntity<>(msg,headers);
+		String answer = restTemplate.postForObject(url, entity, String.class);
+		
+		System.out.println(answer);
+		
+		return answer;
+
+	}
+
+	@Override
+	public boolean control(String msg) {
+		String controlData;
+		// userData 저장
+		boolean result = dao.insertInput(msg);
+		if(result) {
+			// control Data 요청
+			try {
+				controlData = predict(msg);
+			} catch (JsonProcessingException e) {
+				e.printStackTrace();
+				return false;
+			}
+			// control Data 저장
+			 return sendControlData(controlData);
+		}
+		else return false;
 	}	
 }
--- DataPrediction/DataPrediction.py
+++ DataPrediction/DataPrediction.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Response
 import json
+import redis
 
 app = Flask(__name__)
+r = redis.StrictRedis(host="localhost", port=6379, db=0)
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -11,6 +13,47 @@ def predict():
     test_json = json.dumps(test_str)
     #print(test_json)
     return test_json
+
+@app.route('/predict-data', methods=['POST'])
+def save_predict_data():
+    input_json =  request.get_json()
+    #data_dic = {"time":20200811, "data":request.get}
+    json_data_dict = json.dumps(input_json , ensure_ascii=False).encode('utf-8')
+    r.set("predict_data", json_data_dict)
+
+    # get
+    json_test_dict = r.get('predict_data').decode('utf-8')
+    test_dict2 = dict(json.loads(json_test_dict))
+
+    print(test_dict2)
+    '''
+        test_dict = {
+        "foo": "안녕",
+        "goo": ["헬로", "월드"]
+    }
+
+    # set
+    json_test_dict = json.dumps(test_dict, ensure_ascii=False).encode('utf-8')
+    r.set("test_dict", json_test_dict)
+
+    # get
+    json_test_dict = r.get('test_dict').decode('utf-8')
+    test_dict2 = dict(json.loads(json_test_dict))
+
+    print(test_dict2)
+
+    li = [9,8,7]
+
+    r.lpush('test', li)
+    #r.lpush('test', 3)
+    print(r.lrange('test',0,-1))
+
+    r.rpop('test')
+    print(r.lrange('test',0,-1))
+    '''
+    #r.set("one","{\"list\":[1,2,3]}")
+    #print(r.get("one"))
+    return "Success", 200
 
 if __name__ == '__main__':
     app.run(debug=True)
